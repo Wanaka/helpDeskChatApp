ccf9643cc66e0c1af82a059b487428985b0c06c7
bengt.haag <bengt.haag@accenture.com>
2026-07-02 09:41:39 +0200

    helpDeskChatApp: add icons

--- app/src/main/java/com/example/helpdeskchatapp/ui/common/components/QrCodeDialog.kt
+++ app/src/main/java/com/example/helpdeskchatapp/ui/common/components/QrCodeDialog.kt
@@ -1,6 +1,7 @@
 package com.example.helpdeskchatapp.ui.common.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
@@ -17,12 +18,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import com.example.helpdeskchatapp.R
-import com.example.helpdeskchatapp.theme.AndroidGreenMascot
+import com.example.helpdeskchatapp.theme.AndroidGreen
 import com.example.helpdeskchatapp.theme.Dimens
 import com.example.helpdeskchatapp.util.Deeplink
 import com.example.helpdeskchatapp.ui.common.QRCodeGenerator
@@ -46,11 +48,13 @@ fun QrCodeDialog(
     BasicAlertDialog(onDismissRequest = onDismiss) {
         Surface(
             shape = RoundedCornerShape(Dimens.dp16),
-            color = AndroidGreenMascot,
+            color = Color.White,
             tonalElevation = Dimens.dp8
         ) {
             Column(
-                modifier = Modifier.padding(Dimens.dp16),
+                modifier = Modifier
+                    .background(AndroidGreen.copy(alpha = 0.7f))
+                    .padding(Dimens.dp16),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Text(
